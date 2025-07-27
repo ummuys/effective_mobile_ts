21a04fbb10b3fa957ec9d9c00bb9629750a0719f
Ummuys <5percentofmilk@gmail.com>
2025-07-27 14:14:09 +0300

    fix models

--- models/subs.go
+++ models/subs.go
@@ -7,7 +7,7 @@ type SubsRequest struct {
 	Price       int    `json:"price" example:"299"`
 	UserID      string `json:"user_id" example:"123e4567-e89b-12d3-a456-426614174000"`
 	StartDate   string `json:"start_date" example:"07-2025"`
-	EndDate     string `json:"end_date" example:"07-2025 or null (it will be infinity)"`
+	EndDate     string `json:"end_date" example:"optional"`
 }
 
 type Subs struct {
@@ -31,7 +31,7 @@ type SubsResponse struct {
 	Price       int    `json:"price" example:"299"`
 	UserID      string `json:"user_id" example:"123e4567-e89b-12d3-a456-426614174000"`
 	StartDate   string `json:"start_date" example:"07-2025"`
-	EndDate     string `json:"end_date" example:"07-2025 or 12-9999(infinity)"`
+	EndDate     string `json:"end_date" example:"07-2025"`
 }
 
 type ErrorResponse struct {
